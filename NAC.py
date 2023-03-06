d9242fb5eed906e0ec94c3cdd576763888e9ca86
vtzf <tyy2018ustc@gmail.com>
2023-03-06 23:46:29 +0800

    add faster P2P mode

--- NAC.py
+++ NAC.py
@@ -6,7 +6,7 @@ import MPIfunc
 
 def NACcalc():
     if Args.LRANGE:
-        MPIfunc.MPIfunc(
+        MPIfunc.MPIfunc_eq(
             Args.nstep,[0],'int32',[Args.nbands+2],'float64',
             NACfunc.LoadDataNull,NACfunc.SaveE1,
             'all_en.npy','Energy and INICON',NACfunc.ReadE1
@@ -16,44 +16,44 @@ def NACcalc():
         Args.iband_e = int(bandrange[1])
         Args.ibands = Args.iband_e-Args.iband_s+1
     else:
-        MPIfunc.MPIfunc(
+        MPIfunc.MPIfunc_eq(
             Args.nstep,[0],'int32',[Args.nbands],'float64',
             NACfunc.LoadDataNull,NACfunc.SaveE,
             'all_en.npy','Energy',NACfunc.ReadE
         )
 
     if Args.LCHARGE:
-        MPIfunc.MPIfunc(
+        MPIfunc.MPIfunc_eq(
             Args.nstep,[0],'int32',[Args.nbands],'float64',NACfunc.LoadDataNull,
             NACfunc.SaveCD,'all_wht.npy','Charge density',NACfunc.CDInfo
         )
 
     if Args.LPHASE:
-        MPIfunc.MPIfunc(
+        MPIfunc.MPIfunc_eq(
             Args.nstep,[0],'int32',[Args.ibands],'int32',NACfunc.LoadDataNull,
             NACfunc.SavePhase,'phase_m.npy','Phase correction',NACfunc.PhaseInfo
         )
         if Args.LRANGE:
-            MPIfunc.MPIfunc(
+            MPIfunc.MPIfunc_eq(
                 Args.nstep-1,[Args.ibands,Args.ibands],'int32',
                 [Args.ibands,Args.ibands],'float64',NACfunc.LoadDataPhase,
                 NACfunc.SaveNAC,'NATXT','NAC with phase correction',NACfunc.NACPhaseInfo
             )
         else:
-            MPIfunc.MPIfunc(
+            MPIfunc.MPIfunc_eq(
                 Args.nstep-1,[Args.ibands,Args.ibands],'int32',
                 [Args.ibands,Args.ibands],'float64',NACfunc.LoadDataPhase,
                 NACfunc.SaveCOUP,'COUPCAR','NAC with phase correction',NACfunc.NACPhaseInfo
             )
     else:
         if Args.LRANGE:
-            MPIfunc.MPIfunc(
+            MPIfunc.MPIfunc_eq(
                 Args.nstep-1,[0],'int32',[Args.ibands,Args.ibands],'float64',
                 NACfunc.LoadDataNull,NACfunc.SaveNAC,
                 'NATXT','NAC without phase correction',NACfunc.NACInfo
             )
         else:
-            MPIfunc.MPIfunc(
+            MPIfunc.MPIfunc_eq(
                 Args.nstep-1,[0],'int32',[Args.ibands,Args.ibands],'float64',
                 NACfunc.LoadDataNull,NACfunc.SaveCOUP,
                 'COUPCAR','NAC without phase correction',NACfunc.NACInfo
