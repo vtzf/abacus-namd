701beb90b705a35da79a0d54da0a990db787e13b
vtzf <tyy2018ustc@gmail.com>
2023-03-06 23:45:53 +0800

    add faster P2P mode

--- MPIfunc.py
+++ MPIfunc.py
@@ -3,6 +3,7 @@ import time
 from mpi4py import MPI
 import Args
 
+# MPI master-slave mode
 def MPIfunc(
     step,nsend,sendtype,nrecv,recvtype,
     loadfunc,savefunc,savename,task,calfunc
@@ -67,8 +68,8 @@ def MPIfunc(
         print('%s time: %.4fs'%(task,end-start))
 
     else:
+        data_recv = np.empty(nsend,dtype=sendtype)
         while True:
-            data_recv = np.empty(nsend,dtype=sendtype)
             status = MPI.Status()
             comm.Recv([data_recv,None,MPISTYPE],\
                        source=MPI.ANY_SOURCE,\
@@ -80,3 +81,74 @@ def MPIfunc(
             else:
                 break
 
+
+# MPI P2P mode
+def MPIfunc_eq(
+    step,nsend,sendtype,nrecv,recvtype,
+    loadfunc,savefunc,savename,task,calfunc
+):
+    comm = MPI.COMM_WORLD
+    status = MPI.Status()
+    myid = comm.Get_rank()
+    nprocs = comm.Get_size()
+    master = 0
+
+    MPISTYPE = MPI.DOUBLE
+    MPIRTYPE = MPI.DOUBLE
+    if sendtype == 'int32':
+        MPISTYPE = MPI.INT
+    if recvtype == 'int32':
+        MPIRTYPE = MPI.INT
+
+    idx_max = (step*(myid+1))//nprocs
+    idx_min = (step*myid)//nprocs
+    nidx = idx_max - idx_min
+    nidx_max = step//nprocs + 1
+
+    if master == myid:
+        status = MPI.Status()
+        data_send = loadfunc(step,nsend,sendtype)
+        data_recv = np.empty([nidx_max]+nrecv,dtype=recvtype)
+        data = np.zeros([step]+nrecv,dtype=recvtype)
+        null = np.zeros((0),dtype=sendtype)
+
+        start = time.time()
+        idx_min_s = np.zeros((nprocs),dtype='int32')
+        idx_max_s = np.zeros((nprocs),dtype='int32')
+        for i in range(nprocs):
+            idx_max_s[i] = (step*(i+1))//nprocs
+            idx_min_s[i] = (step*i)//nprocs
+        for i in range(1,nprocs):
+            comm.Send([data_send[idx_min_s[i]:idx_max_s[i]],\
+                       None,MPISTYPE],dest=i,tag=1)
+        for i in range(idx_min,idx_max):
+            data[i] = calfunc(i+Args.start_t,data_send[i])
+        for i in range(1,nprocs):
+            status = MPI.Status()
+            comm.Recv([data_recv,None,MPIRTYPE],\
+                       source=MPI.ANY_SOURCE,\
+                       tag=MPI.ANY_TAG,status=status)
+            sender = status.Get_source()
+            data[idx_min_s[sender]:idx_max_s[sender]] = \
+            data_recv[0:idx_max_s[sender]-idx_min_s[sender]]
+            comm.Send([null,None,MPISTYPE],dest=sender,tag=0)
+
+        savefunc(savename,data)
+        end = time.time()
+        print('%s time: %.4fs'%(task,end-start))
+
+    else:
+        data_recv = np.empty([nidx]+nsend,dtype=sendtype)
+        data_send = np.empty([nidx]+nrecv,dtype=recvtype)
+        while True:
+            status = MPI.Status()
+            comm.Recv([data_recv,None,MPISTYPE],\
+                       source=MPI.ANY_SOURCE,\
+                       tag=MPI.ANY_TAG,status=status)
+            num = status.Get_tag()
+            if num != 0:
+                for i in range(nidx):
+                    data_send[i] = calfunc(i+idx_min+Args.start_t,data_recv[i])
+                comm.Send([data_send,None,MPIRTYPE],dest=master,tag=num)
+            else:
+                break
